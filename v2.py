fd1c1ce979b5f57094eb405042c9ea17d93eab6d
Gaëtan <gaetan.hrlt@gmail.com>
2022-04-23 15:20:14 +0200

    Added subscription & cashback

--- v2.py
+++ v2.py
@@ -36,6 +36,8 @@ class Kard:
 		self.account = KardAccount()
 		self.cards = KardCards()
 		self.bank = KardBankAccount()
+		self.subscription = KardSubscription()
+		self.cashback = KardCashback()
 
 
 class KardLocalSecrets(Kard):
@@ -160,7 +162,14 @@ class KardAccount(Kard):
 	@property
 	def complete_data(self):
 		payload = {
-			"query": "query androidMe { me { ... Me_MeParts }}\n\nfragment Card_CardParts on Card { __typename id activatedAt customText name visibleNumber blocked scheme ... on PhysicalCard { atm contactless swipe online design orderedAt }}\n\nfragment Topup_TopupCardParts on TopupCard { id name expirationDate default last4 providerSourceId providerPaymentId}\n\nfragment Me_SubscriptionParts on Subscription { id status cancelledAt cancellationReason nextBilling { date amount { value currency { isoCode name symbol symbolFirst } } } plan { __typename id periodUnit name providerId price { value } }}\n\nfragment Topup_RecurringParts on RecurringPayment { id active amount { value currency { symbol isoCode } } child { id } firstPayment nextPayment cancelledAt topupCard { ... Topup_TopupCardParts }}\n\nfragment Me_TopupRequestParts on TopupRequest { id amount { value currency { symbol isoCode } } reason accepted cancelled declined createdAt requestee { id firstName lastName avatar { url } } requester { id firstName lastName avatar { url } }}\n\nfragment Transaction_TransactionCashbackParts on Transaction { id title amount { value currency { symbol } } image { id url } processedAt category { name color image { url } } ... on CashbackTransaction { cashback { status brandLogo brandName sourceTransaction { id title image { id url } category { name color image { url } } amount { value currency { symbol } } } } }}\n\nfragment Me_MeParts on Me { id modirumId type intercomHashes { android } canFinishOnboarding profile { avatar { url } firstName lastName username age birthday placeOfBirth shippingAddress { firstName lastName street line1 line2 zipcode city state country fullAddress } homeAddress { firstName lastName street line1 line2 zipcode city state country fullAddress } } email emailConfirmed unconfirmedEmail phoneNumber referralCode referralUrl bankAccount { id iban bic user { firstName lastName } balance { value currency { symbol isoCode } } lockedTransactions } card { id } cardBeingSetup { __typename id customText name ... on PhysicalCard { design } } cards { nodes { ... Card_CardParts } } earnings { value currency { symbol isoCode } } onboardingDone pendingDebts { amount { value currency { symbol isoCode } } id owner { avatar { url } firstName id lastName username } reason } topupCards { ... Topup_TopupCardParts } subscription { ... Me_SubscriptionParts } outgoingRecurringPayments { ... Topup_RecurringParts } incomingRecurringPayment { ... Topup_RecurringParts } fundsOrigin canOrderCard externalAuthenticationProviders { id type uniqueId } claimId cardTransactionsCount topupRequestsFromChildren { ... Me_TopupRequestParts } topupRequestsToParent { ... Me_TopupRequestParts } savingsAmount { value } createdAt cashbackEnabled cashbackWallet { id balance { value currency { symbol isoCode } } amountEarned { value currency { symbol isoCode } } transactions(first: 10, order: CREATED, direction: DESC) { pageInfo { endCursor hasNextPage } nodes { ... Transaction_TransactionCashbackParts } } } invitedByOther topupIncentiveActivated hasUnreadActivityItems noPhoneNumber allowedToAddChild currentSpendings { monthlyPos { value } weeklyPos { value } weeklyAtm { value } monthlyAtm { value } } legalSpendingLimits { monthlyPos { value } weeklyPos { value } weeklyAtm { value } monthlyAtm { value } } transactionLimits { id amount { value currency { symbol } } recurrence transactionType } transactionAuthorizations { authorizationType isAuthorized }}",
+			"query": "query androidMe { me { ... Me_MeParts }}\n\n"
+				"fragment Card_CardParts on Card { __typename id activatedAt customText name visibleNumber blocked scheme ... on PhysicalCard { atm contactless swipe online design orderedAt }}\n\n"
+				"fragment Topup_TopupCardParts on TopupCard { id name expirationDate default last4 providerSourceId providerPaymentId}\n\n"
+				"fragment Me_SubscriptionParts on Subscription { id status cancelledAt cancellationReason nextBilling { date amount { value currency { isoCode name symbol symbolFirst } } } plan { __typename id periodUnit name providerId price { value } }}\n\n"
+				"fragment Topup_RecurringParts on RecurringPayment { id active amount { value currency { symbol isoCode } } child { id } firstPayment nextPayment cancelledAt topupCard { ... Topup_TopupCardParts }}\n\n"
+				"fragment Me_TopupRequestParts on TopupRequest { id amount { value currency { symbol isoCode } } reason accepted cancelled declined createdAt requestee { id firstName lastName avatar { url } } requester { id firstName lastName avatar { url } }}\n\n"
+				"fragment Transaction_TransactionCashbackParts on Transaction { id title amount { value currency { symbol } } image { id url } processedAt category { name color image { url } } ... on CashbackTransaction { cashback { status brandLogo brandName sourceTransaction { id title image { id url } category { name color image { url } } amount { value currency { symbol } } } } }}\n\n"
+				"fragment Me_MeParts on Me { id modirumId type intercomHashes { android } canFinishOnboarding profile { avatar { url } firstName lastName username age birthday placeOfBirth shippingAddress { firstName lastName street line1 line2 zipcode city state country fullAddress } homeAddress { firstName lastName street line1 line2 zipcode city state country fullAddress } } email emailConfirmed unconfirmedEmail phoneNumber referralCode referralUrl bankAccount { id iban bic user { firstName lastName } balance { value currency { symbol isoCode } } lockedTransactions } card { id } cardBeingSetup { __typename id customText name ... on PhysicalCard { design } } cards { nodes { ... Card_CardParts } } earnings { value currency { symbol isoCode } } onboardingDone pendingDebts { amount { value currency { symbol isoCode } } id owner { avatar { url } firstName id lastName username } reason } topupCards { ... Topup_TopupCardParts } subscription { ... Me_SubscriptionParts } outgoingRecurringPayments { ... Topup_RecurringParts } incomingRecurringPayment { ... Topup_RecurringParts } fundsOrigin canOrderCard externalAuthenticationProviders { id type uniqueId } claimId cardTransactionsCount topupRequestsFromChildren { ... Me_TopupRequestParts } topupRequestsToParent { ... Me_TopupRequestParts } savingsAmount { value } createdAt cashbackEnabled cashbackWallet { id balance { value currency { symbol isoCode } } amountEarned { value currency { symbol isoCode } } transactions(first: 10, order: CREATED, direction: DESC) { pageInfo { endCursor hasNextPage } nodes { ... Transaction_TransactionCashbackParts } } } invitedByOther topupIncentiveActivated hasUnreadActivityItems noPhoneNumber allowedToAddChild currentSpendings { monthlyPos { value } weeklyPos { value } weeklyAtm { value } monthlyAtm { value } } legalSpendingLimits { monthlyPos { value } weeklyPos { value } weeklyAtm { value } monthlyAtm { value } } transactionLimits { id amount { value currency { symbol } } recurrence transactionType } transactionAuthorizations { authorizationType isAuthorized }}",
 			"variables": {},
 			"extensions": {}
 		}
@@ -409,8 +418,157 @@ class KardBankAccount(Kard):
 		return r['data']['me']['bankAccount']['balance']['value']
 
 
+class KardSubscription(Kard):
+	def __init__(self):
+		super().__init__()
+
+	@property
+	def complete_data(self):
+		payload = {
+			"query": "query androidMe { me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { subscription { id status cancelledAt cancellationReason nextBilling { date amount { value } } plan { __typename id periodUnit name providerId price { value } } } }",
+			"variables":{},
+			"extensions":{}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+		return r['data']['me']['subscription']
+
+	@property
+	def is_active(self):
+		payload = {
+			"query": "query androidMe { me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { subscription { status cancelledAt } }",
+			"variables":{},
+			"extensions":{}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+		return True if r['data']['me']['subscription']['status'] == "ACTIVE" else False
+
+	@property
+	def is_free_karder(self):
+		payload = {
+			"query": "query androidMe { me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { subscription { plan { __typename name } } }",
+			"variables":{},
+			"extensions":{}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+		if r['data']['me']['subscription']['plan']['__typename'] == "LegacyPlan":
+			# double check, probably pointless
+			if r['data']['me']['subscription']['plan']['name'] == "Free Karder":
+				return True
+		return False
+
+	@property
+	def price(self):
+		payload = {
+			"query": "query androidMe { me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { subscription { plan { periodUnit price { value } } } }",
+			"variables":{},
+			"extensions":{}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+		
+		price = r['data']['me']['subscription']['plan']['price']['value']
+		period = r['data']['me']['subscription']['plan']['periodUnit']
+
+		return f"{price}€/{period.lower()}"
+
+	@property
+	def next_billing(self):
+		payload = {
+			"query": "query androidMe { me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { subscription { nextBilling { date amount { value } } } }",
+			"variables":{},
+			"extensions":{}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+		return r['data']['me']['subscription']['nextBilling']
+
+
+class KardCashback(Kard):
+	def __init__(self):
+		super().__init__()
+
+	@property
+	def complete_data(self):
+		payload = {
+			"query": "query androidMe { me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { cashbackEnabled cashbackWallet { id balance { value currency { symbol isoCode } } amountEarned { value currency { symbol isoCode } } transactions(first: 10, order: CREATED, direction: DESC) { pageInfo { endCursor hasNextPage } nodes { ... Transaction_TransactionCashbackParts } } } }"
+				"fragment Transaction_TransactionCashbackParts on Transaction { id title amount { value currency { symbol } } image { id url } processedAt category { name color image { url } } ... on CashbackTransaction { cashback { status brandLogo brandName sourceTransaction { id title image { id url } category { name color image { url } } amount { value currency { symbol } } } } }}",
+			"variables": {},
+			"extensions": {}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+		return r
+
+	@property
+	def is_cashback_active(self):
+		payload = {
+			"query": "query androidMe {me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { cashbackEnabled }",
+			"variables": {},
+			"extensions": {}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+
+		return r['data']['me']['cashbackEnabled']
+
+	@property
+	def total_earned(self):
+		payload = {
+			"query": "query androidMe {me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { cashbackWallet { amountEarned { value currency { symbol isoCode } } } }",
+			"variables": {},
+			"extensions": {}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+
+		return r['data']['me']['cashbackWallet']['amountEarned']['value']
+
+	@property
+	def pending_cashout(self):
+		payload = {
+			"query": "query androidMe {me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { cashbackWallet { balance { value currency { symbol isoCode } } } }",
+			"variables": {},
+			"extensions": {}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+
+		return r['data']['me']['cashbackWallet']['balance']['value']
+
+	@property
+	def transactions(self, limit: int=10):
+		payload = {
+			"query": "query androidMe {me { ... Me_MeParts }}\n\n"
+				"fragment Me_MeParts on Me { cashbackWallet { transactions(first: "+str(limit)+", order: CREATED, direction: DESC) { pageInfo { endCursor hasNextPage } nodes { ... Transaction_TransactionCashbackParts } } } }"
+				"fragment Transaction_TransactionCashbackParts on Transaction { id title amount { value currency { symbol } } image { id url } processedAt category { name color image { url } } ... on CashbackTransaction { cashback { status brandLogo brandName sourceTransaction { id title image { id url } category { name color image { url } } amount { value currency { symbol } } } } }}",
+			"variables": {},
+			"extensions": {}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+
+		return r['data']['me']['cashbackWallet']['transactions']['nodes']
+
+	@property
+	def current_offers(self):
+		payload = {
+			"query": "query androidOffers { cashbackOffers { name url cashbackRate channel description legalTerms isConsumed maxPerUser brand { name logoUrl description } minAmount maxAmount pictureUrl startDate endDate } }",
+			"variables": {},
+			"extensions": {}
+		}
+		r = self.s.post(self.api_host, json=payload).json()
+		return r['data']['cashbackOffers']
+
+		#[{'name': 'SNCF Connect', 'url': 'https://www.sncf-connect.com/home?wiz_medium=part&wiz_source=paylead&wiz_campaign=fr_not__cashback__tr-multiproduit__mk_202204&wiz_content=__cpa', 'cashbackRate': 2.0, 'channel': 'ONLINE', 'description': 'Nouveau client SNCF Connect ? Profite dès maintenant de 2% de cashback sur ta 1ère commande !', 'legalTerms': "Offre de remboursement valable du 08/04/22 au 08/07/22, ou jusqu'à épuisement du budget, pour tout premier achat sur l'application SNCF Connect ou sur le site web https://www.sncf-connect.com", 'isConsumed': False, 'maxPerUser': 1, 'brand': {'name': 'SNCF Connect', 'logoUrl': 'https://static.prod.paylead.fr/images/c163dd86-38a7-48ff-8187-f976cf26cb6e.jpeg?1649343444', 'description': "SNCF Connect, c'est la nouvelle plateforme de voyages tout-en-un : planifiez, réservez et gérez tous vos trajets, petits et grands, en toute sérénité. SNCF Connect vous accompagne de bout en bout, en temps réel et à toutes les étapes de votre voyage."}, 'minAmount': 0.0, 'maxAmount': None, 'pictureUrl': 'https://static.prod.paylead.fr/images/ab97355f-28a5-4f88-8e41-07248061dcea.jpeg?1649413601', 'startDate': '2022-04-08T00:00:00Z', 'endDate': '2022-07-08T21:59:59Z'}]
+
+	def cashout(self):
+		#todo
+		pass
+
 
 
 k = Kard()
 k.init()
-print( k.bank.balance )
+print( k.cashback.current_offers )
